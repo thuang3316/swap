1695f022216c0de2cf89cd89c472474f011fcd12
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2026-01-23 16:28:41 -0500

    refine main.jsx logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from 'react-router'
-import { Home } from './components/Home.jsx'
-import { Shop } from './components/Shop.jsx'
-import { Cart } from './components/Cart.jsx'
-import { Nav } from './components/Nav.jsx'
+import { App } from './components/App.jsx'
 
 import './styling/styles.css'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />
-  },
-  {
-    path: "/cart",
-    element: <Cart />
-  },
-  {
-    path: "/shop",
-    element: <Shop />
-  }
-])
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <div className='flex flex-col gap-4 justify-center items-center'>
-      <RouterProvider router={router}/>
-    </div>
+    <App/>
   </StrictMode>,
 )
